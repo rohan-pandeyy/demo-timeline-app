006ba758aa843afacc3d01855c4d5b7802c6c9c0
ROHAN PANDEY <95585299+rohan-pandeyy@users.noreply.github.com>
2025-08-24 12:23:34 +0530

    Remove unused React import in Timeline.tsx because Vercel throwing error

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
 import { motion, useMotionValue } from "framer-motion";
 
 interface TimelineProps {
